d5286268f744bbd7fca047da9b08fceb6ef6dc3b
Hassan Panahi <hassan.panahi@samim.net>
2024-03-14 14:22:22 +0330

    Add websocket

--- src/rest_boost/include/rest_boost/http_connection.h
+++ src/rest_boost/include/rest_boost/http_connection.h
@@ -11,8 +11,16 @@
 namespace hp {
 namespace http {
 
-using HttpRequestHanlder = std::function<unsigned int (const Methods& method, const std::string& path, std::vector<URIDynamicSection>& inputs, std::string &result)>;
+struct ContentType {
+    std::string type;
+    std::string content;
+};
 
+
+using HttpRequestHanlder = std::function<unsigned int (const Methods& method, const std::string& path, std::vector<URIDynamicSection>& inputs, std::string &result)>;
+using PartsHandler = std::function<std::vector<URIDynamicSection> (const std::string& body, const ContentType& header)>;
+
+//BASED ON https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
 class BoostHTTPConnection : public std::enable_shared_from_this<BoostHTTPConnection>
 {
 public:
@@ -20,18 +28,22 @@ public:
     ~BoostHTTPConnection();
     void start();
 private:
-    void read_request();
-    void process_request();
-    void write_response();
-    void check_deadline();
     HttpRequestHanlder http_handler_;
     boost::asio::ip::tcp::socket socket_;
     boost::beast::flat_buffer buffer_{8192}; //TODO(HP): fix this
+    std::map<std::string, PartsHandler> parts_handler_;
     boost::beast::http::request_parser<boost::beast::http::string_body> request_;
     boost::beast::http::response<boost::beast::http::dynamic_body> response_;
-
     boost::asio::basic_waitable_timer<std::chrono::steady_clock> deadline_;
     static std::map<boost::beast::http::verb, Methods> methods_list_;
+
+    void read_request();
+    void process_request();
+    void write_response();
+    std::vector<URIDynamicSection> form_data_handler(const std::string& body, const ContentType& header);
+    std::vector<URIDynamicSection> application_json_handler(const std::string& body, const ContentType& header);
+    std::vector<URIDynamicSection> x_www_form_urlencoded_handler(const std::string& body, const ContentType& header);
+
 
 };
 
--- src/rest_boost/src/http_connection.cc
+++ src/rest_boost/src/http_connection.cc
@@ -25,6 +25,15 @@ BoostHTTPConnection::BoostHTTPConnection(boost::asio::ip::tcp::socket socket, co
     http_handler_(http_handler), socket_(std::move(socket)), deadline_{socket_.get_executor(), std::chrono::seconds(0)}
 {
     request_.body_limit(boost::optional<std::uint64_t>(1024* 1024 * 1024));
+
+    parts_handler_["text/html"] = std::bind(&BoostHTTPConnection::application_json_handler, this, std::placeholders::_1, std::placeholders::_2);
+    parts_handler_["text/plain"] = std::bind(&BoostHTTPConnection::application_json_handler, this, std::placeholders::_1, std::placeholders::_2);
+    parts_handler_["application/xml"] = std::bind(&BoostHTTPConnection::application_json_handler, this, std::placeholders::_1, std::placeholders::_2);
+    parts_handler_["application/json"] = std::bind(&BoostHTTPConnection::application_json_handler, this, std::placeholders::_1, std::placeholders::_2);
+
+    parts_handler_["multipart/form-data"] = std::bind(&BoostHTTPConnection::form_data_handler, this, std::placeholders::_1, std::placeholders::_2);
+    parts_handler_["application/javascript"] = std::bind(&BoostHTTPConnection::application_json_handler, this, std::placeholders::_1, std::placeholders::_2);
+    parts_handler_["application/x-www-form-urlencoded"] = std::bind(&BoostHTTPConnection::x_www_form_urlencoded_handler, this, std::placeholders::_1, std::placeholders::_2);
 }
 
 BoostHTTPConnection::~BoostHTTPConnection()
@@ -35,54 +44,21 @@ BoostHTTPConnection::~BoostHTTPConnection()
 void BoostHTTPConnection::start()
 {
     read_request();
-    check_deadline();
 }
 
 void BoostHTTPConnection::read_request()
 {
     auto self = shared_from_this();
-
     request_.get().clear();
     boost::beast::http::async_read(socket_, buffer_, request_, [self](boost::beast::error_code ec, std::size_t bytes_transferred)
     {
         boost::ignore_unused(bytes_transferred);
-        if(!ec) {
-
-            std::cout << "trasnfer: " << bytes_transferred  << std::endl;
+        if(!ec)
             self->process_request();
-        }
         else
             std::cout << "error: " << ec.message() << std::endl;
     });
 }
-
-std::vector<std::string> split_data(const std::string& str, const std::string& delimiter)
-{
-    std::vector<std::string> result;
-    std::string data = std::move(str);
-    size_t pos = 0;
-    std::string token;
-    while ((pos = data.find(delimiter)) != std::string::npos) {
-        token = data.substr(0, pos);
-        result.push_back(token);
-        data.erase(0, pos + delimiter.length());
-    }
-    if (data.size() > 0)
-        result.push_back(data);
-    return result;
-}
-
-// Structure to hold each part of the form data
-struct ContentType {
-    std::string type;
-    std::string content;
-};
-
-struct FormDataPart {
-    std::string name;
-    std::string file_name;
-    ContentType header;
-};
 
 ContentType extract_content_type(const std::string& contentType) {
     ContentType header;
@@ -91,7 +67,6 @@ ContentType extract_content_type(const std::string& contentType) {
     auto pos = contentType.find(boundary_title);
     if (pos == std::string::npos) {
         header.type = contentType;
-        std::cout << "throw an exception hp" << std::endl;
     } else {
         pos += boundary_title.size();
         boundary_data = contentType.substr(pos, contentType.size());
@@ -104,79 +79,84 @@ ContentType extract_content_type(const std::string& contentType) {
     return header;
 }
 
-//BASED ON https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
-std::vector<URIDynamicSection> anylize_request_data(const std::string& body, const ContentType& header) {
+std::vector<URIDynamicSection> BoostHTTPConnection::application_json_handler(const std::string& body, const ContentType& header)
+{
+    std::vector<URIDynamicSection> section(1);
+    section[0].type = URISectionType::data;
+    section[0].data = body;
+    section[0].content_type = header.type;
+    return section;
+}
 
-    std::vector<URIDynamicSection> parts;
+std::vector<URIDynamicSection> BoostHTTPConnection::x_www_form_urlencoded_handler(const std::string& body, const ContentType& header)
+{
+    std::vector<URIDynamicSection> section;
+    auto colon_pos = body.find('=');
+    if (colon_pos != std::string::npos) {
+        section.resize(1);
+        section[0].content_type = header.type;
+        section[0].type = URISectionType::parameter;
+        section[0].key = body.substr(0, colon_pos);
+        section[0].value = body.substr(colon_pos + 1, body.length());
+    }
+    return section;
+}
+
+std::vector<URIDynamicSection> BoostHTTPConnection::form_data_handler(const std::string& body, const ContentType& header)
+{
+    std::vector<URIDynamicSection> sections;
+    std::istringstream stream(body);
+    std::string temp_boundary = "--" + header.content;
+    std::string end_boundary = temp_boundary;
+    end_boundary.insert(end_boundary.length() - 1, "--");
     URIDynamicSection current_part;
-    if (header.type != "multipart/form-data" && header.type != "application/x-www-form-urlencoded") {
-        current_part.type = URISectionType::data;
-        current_part.data = body;
-        current_part.content_type = header.type;
-        parts.push_back(current_part);
-    } else if (header.type == "application/x-www-form-urlencoded"){
-        auto colon_pos = body.find('=');
-        current_part.content_type = header.type;
-        current_part.type = URISectionType::parameter;
-        current_part.key = body.substr(0, colon_pos);
-        current_part.value = body.substr(colon_pos + 1, body.length());
-        parts.push_back(current_part);
-    } else {
+    std::string line;
 
-        std::istringstream stream(body);
-        std::string temp_boundary = "--" + header.content;
-        std::string end_boundary = temp_boundary;
-        end_boundary.insert(end_boundary.length() - 1, "--");
-
-        std::string line;
-
-        bool readingContent = false;
-        while (std::getline(stream, line)) {
-            if (line == temp_boundary || line == end_boundary) {
-                if (readingContent) {
-                    current_part.type = URISectionType::data;
-                    current_part.data.pop_back();
-                    parts.push_back(current_part);
-                    current_part = URIDynamicSection();
-                    readingContent = false;
-                }
-            } else if (!readingContent) {
-                // Parse headers
-                auto colon_pos = line.find(':');
-                if (colon_pos != std::string::npos) {
-                    auto header_name = line.substr(0, colon_pos);
-                    // colos_pos + 2 for : the first one is  ':' and the second one ' '
-                    auto header_value = line.substr(colon_pos + 2, line.length() - 1);
-
-                    if (header_name == ContentDispositionString) {
-                        auto name_pos = header_value.find(HeaderNameString);
-                        if (name_pos != std::string::npos) {
-                            auto name_end = header_value.find("\"", name_pos + HeaderNameString.size());
-                            current_part.key = header_value.substr(name_pos + HeaderNameString.size(), name_end - (name_pos + HeaderNameString.size()));
-                        }
-                        auto file_name_pos = header_value.find(HeaderFileNameString);
-                        if (file_name_pos != std::string::npos) {
-                            auto file_name_end = header_value.find("\"", file_name_pos + HeaderFileNameString.size());
-                            current_part.value = header_value.substr(file_name_pos + HeaderFileNameString.size(), file_name_end - (file_name_pos + HeaderFileNameString.size()));
-                        }
-                    } else if (header_name == ContentTypeString) {
-                        if (header_value[header_value.size() - 1] == '\r')
-                            header_value.pop_back();
-                        current_part.content_type = header_value;
-
-                    }
-                } else if (line.empty() || line == "\r") {
-                    readingContent = true;
-                }
-            } else {
-                if (line == "")
-                    current_part.data += "\n";
-                else
-                    current_part.data += line;
+    bool readingContent = false;
+    while (std::getline(stream, line)) {
+        if (line == temp_boundary || line == end_boundary) {
+            if (readingContent) {
+                current_part.type = URISectionType::data;
+                current_part.data.pop_back();
+                sections.push_back(current_part);
+                current_part = URIDynamicSection();
+                readingContent = false;
             }
+        } else if (!readingContent) {
+            auto colon_pos = line.find(':');
+            if (colon_pos != std::string::npos) {
+                auto header_name = line.substr(0, colon_pos);
+                // colos_pos + 2 for : the first one is  ':' and the second one ' '
+                auto header_value = line.substr(colon_pos + 2, line.length() - 1);
+
+                if (header_name == ContentDispositionString) {
+                    auto name_pos = header_value.find(HeaderNameString);
+                    if (name_pos != std::string::npos) {
+                        auto name_end = header_value.find("\"", name_pos + HeaderNameString.size());
+                        current_part.key = header_value.substr(name_pos + HeaderNameString.size(), name_end - (name_pos + HeaderNameString.size()));
+                    }
+                    auto file_name_pos = header_value.find(HeaderFileNameString);
+                    if (file_name_pos != std::string::npos) {
+                        auto file_name_end = header_value.find("\"", file_name_pos + HeaderFileNameString.size());
+                        current_part.value = header_value.substr(file_name_pos + HeaderFileNameString.size(), file_name_end - (file_name_pos + HeaderFileNameString.size()));
+                    }
+                } else if (header_name == ContentTypeString) {
+                    if (header_value[header_value.size() - 1] == '\r')
+                        header_value.pop_back();
+                    current_part.content_type = header_value;
+
+                }
+            } else if (line.empty() || line == "\r") {
+                readingContent = true;
+            }
+        } else {
+            if (line == "")
+                current_part.data += "\n";
+            else
+                current_part.data += line;
         }
     }
-    return parts;
+    return sections;
 }
 
 void BoostHTTPConnection::process_request()
@@ -188,52 +168,36 @@ void BoostHTTPConnection::process_request()
     std::string request_str = ss.str();
 
     auto content_type = req.base()[boost::beast::http::field::content_type];
-
     auto header = extract_content_type(content_type.to_string());
-    auto inputs = anylize_request_data(request_str, header);
-
-    response_.version(request_.get().version());
-    response_.keep_alive(false);
-    auto method = request_.get().method();
-    std::string path = request_.get().target().to_string();
-
-
-
-
-    std::string result = "";
-    Methods new_method = Methods::Unknown;
-    auto method_iterator = methods_list_.find(method);
-    if (method_iterator != methods_list_.end())
-        new_method = method_iterator->second;
-
-    int a = 0;
-//    auto ret = http_handler_(new_method,  result, result);
-    //    response_.result(ret);
-    //    response_.set(boost::beast::http::field::content_type, "text/plain");
-    //    boost::beast::ostream(response_.body()) << result;
-    //    write_response();
+    auto part_handler = parts_handler_.find(header.type);
+    if (part_handler == parts_handler_.end()) {
+        std::cout << "This Part doesn't support: " << header.type << " content: " << header.content << std::endl;
+    } else {
+        auto inputs = part_handler->second(request_str, header);
+        response_.version(request_.get().version());
+        response_.keep_alive(false);
+        auto method = request_.get().method();
+        std::string path = request_.get().target().to_string();
+        std::string result = "";
+        Methods new_method = Methods::Unknown;
+        auto method_iterator = methods_list_.find(method);
+        if (method_iterator != methods_list_.end())
+            new_method = method_iterator->second;
+        auto ret = http_handler_(new_method, path, inputs, result);
+        response_.result(ret);
+        response_.set(boost::beast::http::field::content_type, "text/plain");
+        boost::beast::ostream(response_.body()) << result;
+        write_response();
+    }
 }
 
 void BoostHTTPConnection::write_response()
 {
     auto self = shared_from_this();
-
     boost::beast::http::async_write(socket_, response_, [self](boost::beast::error_code ec, std::size_t)
     {
         self->socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_send, ec);
         self->deadline_.cancel();
-    });
-}
-
-void BoostHTTPConnection::check_deadline()
-{
-    return;
-    auto self = shared_from_this();
-
-    deadline_.async_wait([self](boost::beast::error_code ec)
-    {
-        if(!ec)
-            self->socket_.close(ec);
     });
 }
 
